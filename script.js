7d1da09eb9c21b06893b4db0f48a5943842f335a
Kiraah <KiraahG@gmail.com>
2025-03-24 20:10:23 -0700

    Modified descriptions.

---
+++ script.js
@@ -0,0 +1,20 @@
+let article1 = {
+  title: "Why YOU Should Plant a Victory Garden!",
+  date: 0o5 / 25 / 2025,
+  description:
+    "With the cost of groceries rising, and the destabilization of both the economy at home and abroad, it would be a prescient move on the part of average residents/citizens of the United States to take some of the responsibility for their own food security into their own hands and the hands of those in their communities.",
+};
+let article2 = {
+  title:
+    "Staples To Plant In Your Victory Garden to Save BIG At The Grocery Store!",
+  date: "05/26/2025",
+  description:
+    "These crops have enduring popularity but, depending on where you live, can cost an amr and a leg at the grocery store. That's why you're going to learnt o grow it yourself!",
+};
+let article3 = {
+  title:
+    "10 Things You'd Be Foolish Not to Grow In Your Victory Garden This Spring (2025)!",
+  date: "05/27/2025",
+  description:
+    "These crops grow great in the Spring growing season, so you'd be foolish not to grow them!",
+};
